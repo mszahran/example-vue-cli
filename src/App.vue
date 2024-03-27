be641a677e1f21ca120ea80a330a8cc395d3bf32
Zahran <muhammadzahran32@gmail.com>
2024-03-27 12:08:17 +0700

    memasang fontawesome pada projek

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animate__animated animate__fadeInRight">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+      aspernatur dolores ea eius eos est facere ipsa ipsum libero magni minus
+      nemo nobis, nostrum placeat quis repellat sequi voluptas voluptatem.
+    </p>
+    <font-awesome-icon icon="shoppingCart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
